95cde0295ac3d3eca306e82b280980353ed29249
Patkos Csaba <patkoscsaba@gmail.com>
2014-10-31 11:00:48 +0200

    IntelliJ IDEA (Cassiopeia) IU-139.222.5 <csaba.patkos@csaba.dev.syneto.net Create project.default.xml, PHP.xml, SQL.xml, notifications.xml, filetypes.xml, ui.lnf.xml, Unistore.xml, template.settings.xml, other.xml, Zen XSL.xml, Ruby_Rails.xml, ide.general.xml, _.xml, github_settings.xml, editor.codeinsight.xml, code.style.schemes.xml, PHPUnit.xml, colors.scheme.xml, editor.xml, Deployment.xml, JavaScript.xml, PHP Setter Method.php, keymap.xml, PHP Interface Doc Comment.php, PHP Class.php, PHP Trait.php, git.xml, proxy.settings.xml, webServers.xml, Zen CSS.xml, vcs.xml, NetTuts.xml, Csaba2.icls, path.macros.xml, file.template.settings.xml, Mac OS X 10_5_ copy.xml, Default for KDE copy.xml, xsl.xml, html_xml.xml, print.xml, databaseDrivers.xml, Csaba.icls, Default.xml, PHP Getter Method.php, NetTuts Tutorial.html, debugger.renderers.xml, php.xml, web-browsers.xml, PHP Function Doc Comment.php, Default _1_.xml, abbreviations.xml, user.xml, macros.xml, PHP Interface.php, PHPUnit Test.php, markdown.xml, PHP File.php, Netbeans.icls, Zen HTML.xml

---
+++ $ROOT_CONFIG$/fileTemplates/code/PHP Getter Method.php
@@ -0,0 +1,8 @@
+public ${STATIC} function get${NAME}()
+{
+#if (${STATIC} == "static")
+    return self::$${FIELD_NAME};
+#else
+    return $this->${FIELD_NAME};
+#end
+}
---
+++ $ROOT_CONFIG$/fileTemplates/code/PHP Setter Method.php
@@ -0,0 +1,8 @@
+public ${STATIC} function set${NAME}($${PARAM_NAME})
+{
+#if (${STATIC} == "static")
+    self::$${FIELD_NAME} = $${PARAM_NAME};
+#else
+    $this->${FIELD_NAME} = $${PARAM_NAME};
+#end
+}
